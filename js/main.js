24d727f09a0996237df5253bc9d0108536476efb
xu.miche@gmail.com <xu.miche@gmail.com>
2017-06-15 11:05:16 -0700

    Displayed recommended restaurant images on webpage. #1

--- js/main.js
+++ js/main.js
@@ -18,16 +18,19 @@ function renderRestaurant(restaurant) {
   var $name = document.createElement('h3')
   var $type = document.createElement('p')
   var $price = document.createElement('p')
+  var $image = document.createElement('img')
 
   $name.textContent = restaurant.name
   $type.textContent = restaurant.type
   $price.textContent = restaurant.price
+  $image.setAttribute('src', restaurant.image)
 
   $name.classList.add('name')
 
   $container.appendChild($name)
   $container.appendChild($type)
   $container.appendChild($price)
+  $container.appendChild($image)
 
   return $container
 }
